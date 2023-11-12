0c497e36e4b82b05ea069bf4019d6338d633ef9c
edit <pranjalshukla245@gmail.com>
2023-11-13 03:55:46 +0530

    Mongo atlas

--- models/config.js
+++ models/config.js
@@ -1,9 +1,16 @@
-const mongoose=require('mongoose')
-mongoose.connect('mongodb://localhost:27017/expense-tracker')
-.then(()=>{
-    console.log('Connection open')
-})
-.catch(err=>{
-    console.log('Error')
-    console.log(err)
-})
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  'mongodb+srv://pranjalshukla245:RgiDeemJ5JkE7DGt@cluster0.vtjn0jv.mongodb.net/Expense-tracker?retryWrites=true&w=majority',
+  {
+    useUnifiedTopology: true,
+  }
+)
+  .then(() => {
+    console.log('MongoDB connected successfully');
+    // Continue with your application logic here...
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB');
+    console.error(err);
+  });
